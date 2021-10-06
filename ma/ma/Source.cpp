cf38b3bbd6ca37cd479b60bfb191fd8eccfdef32
sivrena <svinoroeva_m@mail.ru>
2021-10-06 14:47:46 +0300

    Add main function

--- ma/ma/Source.cpp
+++ ma/ma/Source.cpp
@@ -17,6 +17,18 @@ int fib(int f)
 
 int main()
 {
-
+	setlocale(LC_ALL, "Russian");
+	int N;
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int F;
+		std::cout << "Введите число: ";
+		std::cin >> F;
+		if (fib(F))
+			std::cout << "Фибоначчи" << "\n";
+		else
+			std::cout << "Увы" << "\n";
+	}
 	return EXIT_SUCCESS;
 }
